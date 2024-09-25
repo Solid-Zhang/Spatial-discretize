5bb6deced0c44a3f09703ec105cd8ef60ade94e3
张斌 <zb862578964@gmail.com>
2024-09-25 21:09:01 +0800

    main function and workflow

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# 这是一个示例 Python 脚本。
+from osgeo import gdal,ogr
+import numpy as np
+import Raster
+import ZB_hillslope_workflow
+import calc_hand
+import Extract_Basin_by_Type
+import warnings
+import argparse
+import new_discretize
+import util_ZB
+import configparser
+warnings.filterwarnings("ignore")
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
 
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
 
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    # ***************** Stteper 空间离散化 *****************
+    venu = r'F:\空间离散化\代码示例'
+    stream_file = r'F:\空间离散化\代码示例\basin_Stream.tif'
+    dir_file = r'F:\空间离散化\代码示例\basin_dir.tif'
+    DEM_file = r'F:\空间离散化\代码示例\basin_elv.tif'  # DEM
+    Basin_path = r'F:\空间离散化\代码示例\1'  # 子流域shp
+    LU_file=r'F:\空间离散化\代码示例\landuse.tif'
+    Soil_file=r''
+    # util_ZB.Check_self_intersect(Basin_path)
+    # ZB_hillslope_workflow.Clip_DEM_Dir_Stream_sd_hillslope(Basin_path, dir_file, stream_file, DEM_file, LU_file,venu)
+    calc_hand.main(venu, 6, process=3)
+    # new_discretize.merge_HRU(venu)
+    # new_discretize.merge_HLU(venu)
